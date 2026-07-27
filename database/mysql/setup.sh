b9e312c4bd3cd6a80c28a54357c130cc5a486b72
ZaidAlamgir <zaidkhan137782@gmail.com>
2026-07-27 21:01:31 +0530

    Update MySQL recipe download link to stable active release 8.4.10

--- database/mysql/setup.sh
+++ database/mysql/setup.sh
@@ -7,9 +7,9 @@ cd /opt/mysql
 
 echo "Downloading MySQL binary tarball..."
 if command -v wget >/dev/null 2>&1; then
-    wget -O mysql.tar.xz https://dev.mysql.com/get/Downloads/MySQL-8.4/mysql-8.4.2-linux-glibc2.28-x86_64-minimal.tar.xz
+    wget -O mysql.tar.xz https://dev.mysql.com/get/Downloads/MySQL-8.4/mysql-8.4.10-linux-glibc2.28-x86_64-minimal.tar.xz
 else
-    curl -L -o mysql.tar.xz https://dev.mysql.com/get/Downloads/MySQL-8.4/mysql-8.4.2-linux-glibc2.28-x86_64-minimal.tar.xz
+    curl -L -o mysql.tar.xz https://dev.mysql.com/get/Downloads/MySQL-8.4/mysql-8.4.10-linux-glibc2.28-x86_64-minimal.tar.xz
 fi
 
 tar -xf mysql.tar.xz --strip-components=1
