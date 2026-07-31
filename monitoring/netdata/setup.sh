79200cf406b6a2c57b8c9ff1093fae8d9cca760e
ZaidAlamgir <zaidkhan137782@gmail.com>
2026-07-31 21:28:19 +0530

    Implement Netdata static installer downloading v1.46.3 and run targets

--- monitoring/netdata/setup.sh
+++ monitoring/netdata/setup.sh
@@ -2,4 +2,21 @@
 set -e
 echo "=== netdata-monitor Sandbox Setup ==="
 mkdir -p /opt/netdata
+
+echo "Downloading Netdata static binary package..."
+curl -L -o /tmp/netdata-static.gz.run https://github.com/netdata/netdata/releases/download/v1.46.3/netdata-x86_64-v1.46.3.gz.run
+
+echo "Installing Netdata..."
+sh /tmp/netdata-static.gz.run --accept -- --dont-start-it
+
+echo "Setting permissions..."
+if [ -n "$SUDO_UID" ] && [ -n "$SUDO_GID" ]; then
+    chown -R "$SUDO_UID:$SUDO_GID" /opt/netdata
+else
+    chmod -R 777 /opt/netdata
+fi
+
+echo "Cleaning up..."
+rm -f /tmp/netdata-static.gz.run
+
 echo "=== netdata-monitor Sandbox Forged ==="
