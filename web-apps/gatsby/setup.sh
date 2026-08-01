ee664d05cbd8b350d2df89514a7df0c9d1ab816c
ZaidAlamgir <zaidkhan137782@gmail.com>
2026-08-01 16:35:21 +0530

    Implement Gatsby sandbox container with local npm install of gatsby-cli

--- web-apps/gatsby/setup.sh
+++ web-apps/gatsby/setup.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 set -e
 echo "=== gatsby-builder Sandbox Setup ==="
-echo 'Gatsby tools forged'
+mkdir -p /opt/gatsby
+cd /opt/gatsby
+
+echo "Installing gatsby-cli locally..."
+npm install gatsby-cli
+
+echo "Setting permissions..."
+if [ -n "$SUDO_UID" ] && [ -n "$SUDO_GID" ]; then
+    chown -R "$SUDO_UID:$SUDO_GID" /opt/gatsby
+else
+    chmod -R 777 /opt/gatsby
+fi
+
 echo "=== gatsby-builder Sandbox Forged ==="
